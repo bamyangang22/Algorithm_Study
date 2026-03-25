290512c88c8fb08e89dba55e99799c00189e1f56
bamyangang22 <165766960+bamyangang22@users.noreply.github.com>
2026-03-25 15:38:17 +0900

    [level 1] Title: 폰켓몬, Time: 0.74 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -0,0 +1,17 @@
+# 해당 풀이 예상 시간 복잡도 O(n), n = nums 길이(n^4)
+def solution(nums):
+    
+    # 1. 타입 중복 제거 O(n)
+    type_set = set(nums)
+    # 2. 타입 종류 확인 O(1)
+    type_count = len(type_set)
+    # 3. 최대 선택 갯수 확인 O(1)
+    # 만약 nums 길이가 짝수가 아니었으면 내림이 되니까 그런 케이스일 경우 주의!
+    max_pick = len(nums)/2
+    
+    # 4. 가장 많은 종류 선택 로직 O(1)
+    return min(type_count, max_pick)
+    # if type_count >= max_pick:
+    #     return max_pick
+    # else:
+    #     return type_count
