a5ebe62e5f3211a4cecdf37419c5c5db18a8a6ce
bamyangang22 <165766960+bamyangang22@users.noreply.github.com>
2026-04-27 02:37:02 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,17 @@
+def solution(my_string):
+    answer = ''
+    for ch in my_string:
+        if (ch == "a"):
+            continue
+        elif (ch == "e"):
+            continue
+        elif (ch == "i"):
+            continue
+        elif (ch == "o"):
+            continue
+        elif (ch == "u"):
+            continue
+        else:
+            answer += ch
+            
+    return answer
