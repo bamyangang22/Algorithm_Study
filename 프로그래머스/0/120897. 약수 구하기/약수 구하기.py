f673329201890ae43798ed3973f64cbbbf23ef42
bamyangang22 <165766960+bamyangang22@users.noreply.github.com>
2026-04-28 11:40:52 +0900

    [level 0] Title: 약수 구하기, Time: 1.03 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
+++ 프로그래머스/0/120897. 약수 구하기/약수 구하기.py
@@ -1,12 +1,6 @@
 def solution(n):
     answer = []
-    for i in range(1, int(n**0.5)+1):
-        if (n % i == 0):
+    for i in range(1, n + 1):
+        if(n % i == 0):
             answer.append(i)
-    rev_fac = sorted(answer, reverse = True)
-    for num in rev_fac:
-        if(num**2 == n):
-            continue
-        temp = n / num
-        answer.append(temp)
     return answer
