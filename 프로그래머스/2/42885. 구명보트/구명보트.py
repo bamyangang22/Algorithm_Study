fa8620678d2a211f2af6f7bdfc1ad28b1d7e2270
bamyangang22 <165766960+bamyangang22@users.noreply.github.com>
2026-05-23 15:56:03 +0900

    [level 2] Title: 구명보트, Time: 8.49 ms, Memory: 9.47 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,16 +1,16 @@
 def solution(people, limit):
-    cnt = 0
     left = 0
     right = len(people) - 1
+    cnt = 0
     people.sort()
     while left <= right:
         if(people[left] + people[right] <= limit):
-            cnt += 1
             left += 1
-            right -=1
-        elif(people[left] + people[right] > limit):
+            right -= 1
             cnt += 1
-            right -=1
+        else:
+            right -= 1
+            cnt += 1
     return cnt
 
 # def solution(people, limit):
@@ -32,3 +32,17 @@ def solution(people, limit):
 #         boat_cnt +=1
         
 #     return boat_cnt
+
+# cnt = 0
+#     left = 0
+#     right = len(people) - 1
+#     people.sort()
+#     while left <= right:
+#         if(people[left] + people[right] <= limit):
+#             cnt += 1
+#             left += 1
+#             right -=1
+#         elif(people[left] + people[right] > limit):
+#             cnt += 1
+#             right -=1
+#     return cnt
